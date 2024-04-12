810d34b8fea21dadf8ea6624aabe8bcd3109fc87
kk9 <kirtikumar_p@tekditechnologies.com>
2024-04-12 16:19:58 +0530

    Update Details.tsx

--- components/ui/Details.tsx
+++ components/ui/Details.tsx
@@ -53,9 +53,9 @@ const Details = ({ category }: { category: Category }) => {
 const DetaillLink = ({ title, url }: { title: string; url: string }) => {
   return (
     <Link href={url} target="_blank" className="h-full">
-      <div className="bg-[#E2D3C6] font-Rubik text-lg lg:text-4xl font-bold flex items-center justify-between text-[#511917] p-8 rounded-xl w-full h-full">
+      <div className="bg-[#E2D3C6] font-Rubik text-lg lg:text-2xl font-bold flex items-center justify-between text-[#511917] p-6 rounded-xl w-full h-full">
         {title}
-        <Image src={"./icons/PopUp-01.svg"} height={30} width={30} alt="open link" />
+        <Image src={"./icons/PopUp-01.svg"} height={20} width={20} alt="open link" />
       </div>
     </Link>
   );
