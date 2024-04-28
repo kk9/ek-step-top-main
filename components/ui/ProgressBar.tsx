aaaf6ab6c7714d376742ec8d8e226f2491c9273f
kk9 <kirtikumar_p@tekditechnologies.com>
2024-04-28 22:25:06 +0530

    Update ProgressBar.tsx

--- components/ui/ProgressBar.tsx
+++ components/ui/ProgressBar.tsx
@@ -8,17 +8,17 @@ const ProgressBar = () => {
 
   const setCurrent = useCategoryStore((state) => state.setCurrent);
   return (
-    <div className="absolute bottom-0 w-screen p-10 pb-[3.5rem] font-Rubik font-bold text-white bg-gradient-to-t from-[#554342] via-[#554342] to-transparent">
+    <div className="mb-progress absolute bottom-0 w-screen p-10 pb-[3.5rem] font-Rubik font-bold text-white bg-gradient-to-t from-[#554342] via-[#554342] to-transparent">
       <div className="flex w-full cursor-pointer">
         {introDone &&
           categories.map((category, index) => (
             <div className={"flex flex-col items-center justify-center w-[15%] "} key={index} onClick={() => setCurrent(category.name)}>
-              <div className="shadow text-sm lg:text-lg h-4 mb-4">{index + 1}</div>
+              <div className="shadow text-sm lg:text-lg h-4 mb-4 mb-font">{index + 1}</div>
               <div
                 key={index}
                 className={`${current === category.name ? "bg-white" : ""} h-2 border-y border-white w-full mb-1 ${index === 0 ? "border-l" : ""} ${index === categories.length - 1 ? "border-r" : ""}`}
               ></div>
-              <div className="shadow text-sm lg:text-lg h-4">{category.name}</div>
+              <div className="shadow text-sm lg:text-lg h-4 mb-font">{category.name}</div>
             </div>
           ))}
       </div>
