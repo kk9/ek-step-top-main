83186ffac2ff0d80efc1b6ab1574d359c18df3c9
kk9 <kirtikumar_p@tekditechnologies.com>
2024-04-28 22:22:08 +0530

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -8,15 +8,15 @@ const Loading = () => {
         <Image src="./logos/EkStep_logo_v2.svg" width={120} height={100} alt="ek step logo" className="object-cover z-50" />
       </div>
       <div className="w-screen h-screen flex flex-col items-center justify-center around gap-4 z-50 absolute">
-        <div className="font-Rubik absolute top-40 right-[53%] translate-x-1/2 text-3xl z-10 opacity-50 text-white">
+        <div className="font-Rubik absolute top-40 right-[53%] translate-x-1/2 text-3xl z-10 opacity-50 text-white w90 mob-wel-come">
           WELCOME TO THE
         </div>
       </div>
-      <div  className="z-50 absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 mt-30">
+      <div  className="z-50 absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 mt-30 w90">
         <Image src="./logos/People+Logo.svg" alt="people+ logo" width={400} height={300} className="z-50 object-cover" />
       </div>
 
-      <div className="text-white font-Albert absolute top-[70%] right-[53%] -translate-x-1/2 animate-pulse">
+      <div className="text-white font-Albert absolute top-[70%] right-[53%] -translate-x-1/2 animate-pulse w90">
         LOADING
       </div>
     </main>
